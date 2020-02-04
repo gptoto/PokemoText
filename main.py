bb9fb52a3df65b4cbe4bcdc17fbed3bb2868af54
gptoto <hamonaxe@live.fr>
2020-02-04 01:24:32 +0100

    Add of the Pokemon Text Project

---
+++ main.py
@@ -0,0 +1,8 @@
+import menu
+from etat_jeu import Etat_jeu
+
+Etat_jeu == Etat_jeu.ENCOURS
+
+while Etat_jeu == Etat_jeu.ENCOURS:
+    print("[=====================================================]")
+    # menu.print_menu()
