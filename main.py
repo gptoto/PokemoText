fa85169634df719e26c09394171eec3f7b1f145d
Hamon <hamonaxel@live.fr>
2020-02-04 09:39:28 +0100

    Main add (Pokemon menu with fight/capture/shop) option.
    Add Magasin.py file (empty)

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-import menu
-from etat_jeu import Etat_jeu
+import os
 
-Etat_jeu == Etat_jeu.ENCOURS
+print("                                                                                       ")
+print("                                                                                       ")
+print("[=================================JEU TEXTUEL POKEMON=================================]")
+print("|                                                                                     |")
+print("|                                                                                     |")
+print("|                                  1) Combat Pokémon.                                 |")
+print("|                                  2) Capture de Pokémon.                             |")
+print("|                                  3) Magasin.                                        |")
+print("|                                                                                     |")
+print("[=====================================================================================]")
+print("                                                                                       ")
 
-while Etat_jeu == Etat_jeu.ENCOURS:
-    print("[=====================================================]")
-    # menu.print_menu()
+while True:
+    choix = int(input("                                       Que faire ?"))
+
+    if (choix == 1):
+        exec(open("combat.py").read())
+        # Clear de la console
+        clear = lambda: os.system('cls')
+        clear()
+    elif (choix == 2):
+        exec(open("capture.py").read())
+        # Clear de la console
+        clear = lambda: os.system('cls')
+        clear()
+    elif (choix == 3):
+        exec(open("magasin.py").read())
+        # Clear de la console
+        clear = lambda: os.system('cls')
+        clear()
+    else:
+        while (choix > 2):
+            print("Veuillez selectionner un choix valide.")
+            choix = int(input("                                       Que faire ?"))
+            False
