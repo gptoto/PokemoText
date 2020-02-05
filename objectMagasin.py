c81067b1737c859febc70715997edf974fd36359
gptoto <hamonaxe@live.fr>
2020-02-06 00:07:37 +0100

    Modification on "joueur.py" and "objectMagasin" in order to integrate player's money in it.

--- objectMagasin.py
+++ objectMagasin.py
@@ -2,6 +2,9 @@ import sys
 import time
 import keyboard
 
+from joueur import Joueur
+
+
 def delay_print(s):
     # Affiche petit à petit un élément
     for c in s:
@@ -9,29 +12,35 @@ def delay_print(s):
         sys.stdout.flush()
         time.sleep(0.05)
 
-class ObjectMagasin():
 
-    def __init__(self,name,price):
+class ObjectMagasin:
+
+    def __init__(self, name, price):
         self.name = name
         self.price = price
 
     def vente(self):
         print("")
 
-if __name__ == '__main__':
-    #Objets
-    Pokeball = ObjectMagasin("Pokeball",250)
-    Superball = ObjectMagasin("Superball",600)
-    Hyperball = ObjectMagasin("Hyperball",1000)
 
-    ObjectList = [Pokeball,Superball,Hyperball] # Insert à Rendre automatique
+if __name__ == '__main__':
+    # Joueur actuel
+    Red = Joueur('Red',1000)
+
+    # Objets
+    Pokeball = ObjectMagasin("Pokeball", 250)
+    Superball = ObjectMagasin("Superball", 600)
+    Hyperball = ObjectMagasin("Hyperball", 1000)
+
+    ObjectList = [Pokeball, Superball, Hyperball]  # Insert à Rendre automatique
     print("\n[================================= MAGASIN =================================]")
 
-    i = 1 # Indice dans la liste
+    i = 1  # Indice dans la liste
     for element in range(len(ObjectList)):
         ObjectList.append(element)
         # Ajout de ligne dynamique
-        print(f"                             {i}) {ObjectList[element].name} : {ObjectList[element].price} $                           ")
+        print(
+            f"                             {i}) {ObjectList[element].name} : {ObjectList[element].price} $                           ")
         i = i + 1
 
     print("                                                                                       ")
@@ -42,14 +51,24 @@ if __name__ == '__main__':
 
     # Gérer inventaire
     # Si ESC, quitter
+
+
     while True:
         if keyboard.is_pressed('Esc'):
             exec(open("main.py").read())
         # Si un objet est présent dans la liste
-        elif (choix < len(ObjectList)):
-            # Soustraire montant à argent du joueur
-            print("Selectionner valeur de l'item acheté et soustraire argent.")
-            break
+        elif choix < len(ObjectList):
+            nomBall = ObjectList[choix].name
+            prixBall = ObjectList[choix].price
+
+            if Red.argent < prixBall:
+                print("Solde insuffisant.")
+            else:
+                print( nomBall+" achetÃ©(e)")
+                # Soustraire montant de la balle à argent du joueur
+                Red.argent = Red.argent - prixBall
+                time.sleep(5)
+                break
         else:
             # Ne passe pas dedans
             print("Veuillez entrer une réponse valide.")
